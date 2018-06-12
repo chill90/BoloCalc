Updated channel.py to fix units bug

src/channel.py
@@ -35,17 +35,17 @@ class Channel:
                        'Yield':             self.__paramSamp(pr.Parameter(self.log, 'Yield',             self.dict['Yield'],                min=0.0, max=1.0   ), self.bandID),
                        'Pixel Size':        self.__paramSamp(pr.Parameter(self.log, 'Pixel Size',        self.dict['Pixel Size'], un.mmToM, min=0.0, max=np.inf), self.bandID),
                        'Waist Factor':      self.__paramSamp(pr.Parameter(self.log, 'Waist Factor',      self.dict['Waist Factor'],         min=2.0, max=np.inf), self.bandID),
-                       'Band Center':       pr.Parameter(self.log, 'Band Center',     self.dict['Band Center'], un.GHzToHz,   min=0.0, max=np.inf),
-                       'Fractional BW':     pr.Parameter(self.log, 'Fractional BW',   self.dict['Fractional BW'],             min=0.0, max=2.0   ),
-                       'Det Eff':           pr.Parameter(self.log, 'Det Eff',         self.dict['Det Eff'],                   min=0.0, max=1.0   ),
-                       'Psat':              pr.Parameter(self.log, 'Psat',            self.dict['Psat'],                      min=0.0, max=np.inf),
-                       'Psat Factor':       pr.Parameter(self.log, 'Psat Factor',     self.dict['Psat Factor'],               min=0.0, max=np.inf),
-                       'Carrier Index':     pr.Parameter(self.log, 'Carrier Index',   self.dict['Carrier Index'],             min=0.0, max=np.inf),
-                       'Tc':                pr.Parameter(self.log, 'Tc',              self.dict['Tc'],                        min=0.0, max=np.inf),
-                       'Tc Fraction':       pr.Parameter(self.log, 'Tc Fraction',     self.dict['Tc Fraction'],               min=0.0, max=np.inf),
-                       'SQUID NEI':         pr.Parameter(self.log, 'SQUID NEI',       self.dict['SQUID NEI'],                 min=0.0, max=np.inf),
-                       'Bolo Resistance':   pr.Parameter(self.log, 'Bolo Resistance', self.dict['Bolo Resistance'],           min=0.0, max=np.inf),
-                       'Read Noise Frac':   pr.Parameter(self.log, 'Read Noise Frac', self.dict['Read Noise Frac'],           min=0.0, max=1.0   )}
+                       'Band Center':       pr.Parameter(self.log, 'Band Center',     self.dict['Band Center'], un.GHzToHz,     min=0.0, max=np.inf),
+                       'Fractional BW':     pr.Parameter(self.log, 'Fractional BW',   self.dict['Fractional BW'],               min=0.0, max=2.0   ),
+                       'Det Eff':           pr.Parameter(self.log, 'Det Eff',         self.dict['Det Eff'],                     min=0.0, max=1.0   ),
+                       'Psat':              pr.Parameter(self.log, 'Psat',            self.dict['Psat'], un.pWtoW,              min=0.0, max=np.inf),
+                       'Psat Factor':       pr.Parameter(self.log, 'Psat Factor',     self.dict['Psat Factor'],                 min=0.0, max=np.inf),
+                       'Carrier Index':     pr.Parameter(self.log, 'Carrier Index',   self.dict['Carrier Index'],               min=0.0, max=np.inf),
+                       'Tc':                pr.Parameter(self.log, 'Tc',              self.dict['Tc'],                          min=0.0, max=np.inf),
+                       'Tc Fraction':       pr.Parameter(self.log, 'Tc Fraction',     self.dict['Tc Fraction'],                 min=0.0, max=np.inf),
+                       'SQUID NEI':         pr.Parameter(self.log, 'SQUID NEI',       self.dict['SQUID NEI'], un.pArtHzToArtHz, min=0.0, max=np.inf),
+                       'Bolo Resistance':   pr.Parameter(self.log, 'Bolo Resistance', self.dict['Bolo Resistance'],             min=0.0, max=np.inf),
+                       'Read Noise Frac':   pr.Parameter(self.log, 'Read Noise Frac', self.dict['Read Noise Frac'],             min=0.0, max=1.0   )}
         #Store the camera parameters
         self.camElv    = camera.params['Boresight Elevation']
         self.optCouple = camera.params['Optical Coupling']