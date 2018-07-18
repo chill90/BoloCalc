2ba96b38d2674246977c9efdcd8e3a11608a8cc9
Charlie Hill <chill90@berkeley.edu>
2018-07-17 18:31:01 -0700

    Updated gitignore and init.sh

--- init.sh
+++ init.sh
@@ -12,14 +12,6 @@ wget http://pbfs.physics.berkeley.edu/BoloCalc/ATM/atm.zip
 unzip atm.zip -d src/
 rm atm.zip
 
-#Create Experiment/ directory if it doesn't already exist
-if [ -d "$Experiments/" ]
-then
-    echo "Experiments/ directory already exists"
-else
-    mkdir Experiments/
-fi
-
 #Download example experiment
 wget http://pbfs.physics.berkeley.edu/BoloCalc/EX/ex.zip
 unzip ex.zip -d Experiments/
