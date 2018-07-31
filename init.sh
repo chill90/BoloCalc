217a2aa5246c9a1c8b15a5009a167408a5db4446
Charlie Hill <chill90@berkeley.edu>
2018-07-30 17:42:39 -0700

    Testing init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,35 @@
+#Check which operating system you are on
+if [ "$(uname)" == "Darwin" ]; then
+    # Mac OSX
+    OS='Linux'
+    RM_CMD='rm'
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Linux
+    OS='Linux'
+    RM_CMD='rm'
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Windows 32-bit
+    OS='Windows'
+    RM_CMD='del'
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Windows 64-bit
+    OS='Windows'
+    RM_CMD='del'
+fi
+
 #Install wget
-WGETLOC=$(which wget)
-if [ -z "$WGETLOC" ]
+if [ OS == 'Linux' ]
 then
-    echo
-    echo "ERROR: wget needs to be installed to download auxiliary files"
-    echo
-    exit 1
-else
-    echo "wget located at ${WGETLOC}"
+    WGETLOC=$(which wget)
+    if [ -z "$WGETLOC" ]
+    then
+	echo
+	echo "ERROR: wget needs to be installed to download auxiliary files"
+	echo
+	exit 1
+    else
+	echo "wget located at ${WGETLOC}"
+    fi
 fi
 
 #Download atm files
@@ -16,9 +38,13 @@ then
     echo
     echo "NOTE: src/atmFiles/ already exists. Do 'rm -r src/atmFiles/' to remove before re-downloading"
 else
+    if [ -f "atm.zip" ]
+    then
+	${RM_CMD} atm.zip
+    fi
     wget http://pbfs.physics.berkeley.edu/BoloCalc/ATM/atm.zip
     unzip atm.zip -d src/
-    rm atm.zip
+    ${RM_CMD} atm.zip
 fi
 
 #Download example experiment
@@ -27,9 +53,13 @@ then
     echo
     echo "NOTE: Experiments/ExampleExperiment/ already exists. Do 'rm -r Experiments/ExampleExperiment/' to remove before re-downloading"
 else
+    if [ -f "ex.zip" ]
+    then
+	${RM_CMD} ex.zip
+    fi
     wget http://pbfs.physics.berkeley.edu/BoloCalc/EX/ex.zip
     unzip ex.zip -d Experiments/
-    rm ex.zip
+    ${RM_CMD} ex.zip
 fi
 
 echo
