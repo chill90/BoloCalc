1a61164bc472ac5af4f5d71042df04c677f2b0d7
Charles Hill <charles.a.hill01@gmail.com>
2020-11-19 09:03:10 -0800

    Replaced shell scripts with python scripts

---
+++ init.py
@@ -0,0 +1,47 @@
+import shutil as su
+import sys as sy
+import os
+
+# Install python packages
+curr_dir = os.path.dirname(__file__)
+install_file = os.path.join(
+    curr_dir, 'auxil', 'install_packages.py')
+if sy.version_info[0] == 3:
+    os.system("python %s" % (install_file))
+else:
+    sy.stdout.write("\n***** Python 2 is no longer supported for "
+                    "BoloCalc v0.10 (Sep 2019) and beyond *****\n\n")
+    sy.exit()
+
+# Require wget
+if su.which('wget') is None:
+    sy.stdout.write(
+        "\nERROR: wget needs to be installed to download auxiliary files\n\n")
+    sy.exit()
+
+# Download example experiment
+example_dir = os.path.join(
+    curr_dir, "Experiments", "ExampleExperiment")
+if os.path.exists(example_dir):
+    sy.stdout.write(
+        ("\nNOTE: Experiments" + os.sep + "ExampleExperiment" +
+        os.sep + " already exists. If you want a fresh copy, "
+        "please use Experiments" + os.sep + "importExperiments.py.\n\n"))
+else:
+    ex_zip = os.path.join(curr_dir, "ex.zip")
+    if os.path.exists(ex_zip):
+        os.remove(ex_zip)
+    os.system("wget http://pbfs.physics.berkeley.edu/BoloCalc/EX/ex.zip")
+    os.system(("unzip ex.zip -d Experiments", os.sep))
+    os.remove(ex_zip)
+
+# Download atmosphere files
+update_atm_file = os.path.join(
+    os.path.dirname(__file__), "update_atm.py")
+os.system("python %s" % (update_atm_file))
+atm_files_dir = os.path.join(
+    os.path.dirname(__file__), "src", "atmFiles")
+if os.path.exists(atm_files_dir):
+    sy.stdout.write(
+        "\nNOTE: src" + os.sep + "atmFiles" + os.sep, " is no longer used by "
+        "BoloCalc. We suggest removing it.\n\n")
---
+++ update_atm.py
@@ -0,0 +1,27 @@
+import shutil as su
+import sys as sy
+import os
+
+# Require wget
+if su.which('wget') is None:
+    sy.stdout.write(
+        "\nERROR: wget needs to be installed to download atmosphere files\n")
+    sy.exit()
+
+# Download atmosphere files
+fname = "atm_20200916.hdf5"
+atm_file = os.path.join(
+    os.path.dirname(__file__), fname)
+new_atm_file = os.path.join(
+    os.path.dirname(__file__), "src", fname)
+os.system("wget http://pbfs.physics.berkeley.edu/BoloCalc/ATM/%s" % (fname))
+if os.path.exists(atm_file):
+    sy.stdout.write(
+        "\nSuccessfully downloaded atmosphere file %s." % (fname))
+    sy.stdout.write(
+        ("\nADVICE: delete old atm files (~1 GB each) from BoloCalc" + 
+        os.sep + "src" + os.sep + "\n\n"))
+    os.rename(atm_file, new_atm_file)
+else:
+    sy.stdout.write(
+        "\nERROR: problem downloading atmosphere file %s\n\n" % (fname))
